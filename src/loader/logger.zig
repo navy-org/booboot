0cbf3d275f722b0628565299513522b9da924b5e
keyboard-slayer <git@keyb.moe>
2025-07-10 14:19:06 +0200

    feat: add logger

--- src/loader/logger.zig
+++ src/loader/logger.zig
@@ -13,3 +13,34 @@ fn uefiWriteOpaque(_: *const anyopaque, bytes: []const u8) !usize {
 }
 
 pub const uefiWriter = std.io.AnyWriter{ .context = @ptrFromInt(0xdeadbeef), .writeFn = uefiWriteOpaque };
+
+pub fn log(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    if (uefi.system_table.con_out) |cout| {
+        const color = comptime switch (level) {
+            .debug => .blue,
+            .info => .green,
+            .warn => .yellow,
+            .err => .red,
+        };
+
+        const text = comptime switch (level) {
+            .debug => "DEBUG",
+            .info => "INFO",
+            .warn => "WARN",
+            .err => "ERROR",
+        };
+
+        const prefix = if (scope != .default) @tagName(scope) else "";
+        cout.setAttribute(.{ .foreground = color }) catch {};
+        uefiWriter.print("{s: >6}", .{text}) catch {};
+        cout.setAttribute(.{ .foreground = .white }) catch {};
+        uefiWriter.print("{s: <7}", .{prefix}) catch {};
+        uefiWriter.print(format, args) catch {};
+        uefiWriter.print("\r\n", .{}) catch {};
+    }
+}
--- src/loader/root.zig
+++ src/loader/root.zig
@@ -1,7 +1,13 @@
-const uefi = @import("std").os.uefi;
-const writer = @import("./logger.zig").uefiWriter;
+const std = @import("std");
+const logger = @import("./logger.zig").log;
+const uefi = std.os.uefi;
+
+pub const std_options: std.Options = .{
+    .log_level = .debug,
+    .logFn = logger,
+};
 
 pub fn main() void {
-    _ = writer.print("Hello, World!", .{}) catch {};
+    std.log.info("Hello, World!", .{});
     _ = uefi.system_table.boot_services.?.stall(5 * 1000 * 1000);
 }
