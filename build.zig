72e8a28c255287c5eb621f3de018c19fb120d547
keyboard-slayer <git@keyb.moe>
2025-07-10 21:48:19 +0200

    feat: add file open + config parsing (only on boot disk)

--- build.zig
+++ build.zig
@@ -1,26 +1,9 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
-    const target = b.resolveTargetQuery(.{
-        .cpu_arch = std.Target.Cpu.Arch.x86_64,
-        .os_tag = std.Target.Os.Tag.uefi,
-        .abi = std.Target.Abi.msvc,
-    });
-    const optimize = b.standardOptimizeOption(.{});
-    const loader = b.addExecutable(.{
-        .name = "booboot",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/loader/root.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{},
-        }),
-    });
-
-    b.installArtifact(loader);
-
+fn runDemo(b: *std.Build, loader: *std.Build.Step.Compile) !void {
     const runStep = b.step("run", "Run the project");
     const buildStep = b.addInstallArtifact(loader, .{});
+    const configCopy = b.addInstallFile(b.path("sample/loader.json"), "./sysroot/loader.json");
     const efiCopy = b.addInstallFile(loader.getEmittedBin(), "./sysroot/efi/boot/bootx64.efi");
     const fetchBios = b.addSystemCommand(&.{ "curl", "-C", "-", "https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF.fd", "-o", "./zig-out/bios.fd" });
     const qemuStep = b.addSystemCommand(&.{
@@ -40,5 +23,27 @@ pub fn build(b: *std.Build) !void {
     efiCopy.step.dependOn(&buildStep.step);
     fetchBios.step.dependOn(&efiCopy.step);
     qemuStep.step.dependOn(&fetchBios.step);
+    qemuStep.step.dependOn(&configCopy.step);
     runStep.dependOn(&qemuStep.step);
+}
+
+pub fn build(b: *std.Build) !void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = std.Target.Cpu.Arch.x86_64,
+        .os_tag = std.Target.Os.Tag.uefi,
+        .abi = std.Target.Abi.msvc,
+    });
+    const optimize = b.standardOptimizeOption(.{});
+    const loader = b.addExecutable(.{
+        .name = "booboot",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("./src/loader/root.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{},
+        }),
+    });
+
+    try runDemo(b, loader);
+    b.installArtifact(loader);
 }
